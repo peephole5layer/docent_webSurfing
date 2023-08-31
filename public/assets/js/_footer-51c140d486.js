54d956dd714877e8d8673d5d366d0f132c26f1aa
bhav380 <bhaveshrathour380@gmail.com>
2023-08-31 11:39:23 +0530

    jj

---
+++ public/assets/js/_footer-51c140d486.js
@@ -0,0 +1,1 @@
+console.log("hello i am footer dudj");let footer=document.querySelector(".footer"),bgUrl=footer.getAttribute("data-backgroundUrl");console.log(bgUrl+"Hidiid"),footer.style.background=`url(${bgUrl}) no-repeat`,footer.style.backgroundSize="cover",footer.style.backgroundPositionX="center",footer.style.backgroundPositionY="bottom";
